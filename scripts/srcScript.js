4a320d3bde9fc0854efa6faf4815a368b5cdb27c
doomedboi <73707198+doomedboi@users.noreply.github.com>
2021-05-26 16:31:12 +1000

    added blood and some mooving
    feel like i'm working right now

--- scripts/srcScript.js
+++ scripts/srcScript.js
@@ -1,61 +1,104 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
+cvs.width = innerWidth;
+cvs.height = innerHeight;
 // load images
 
 var bird = new Image();
 var bg = new Image();
 var fg = new Image();
-
+var kurt = new Image();
 
 bird.src = "images/bird.png";
 bg.src = "images/bg.png";
 fg.src = "images/fg.png";
-
-
+kurt.src = "images/kurt.png";
 
 class Entity {
-    constructor(x, y, sprite){
+    constructor(x, y, sprite, hitbox){
         this.x = x
         this.y = y
         this.sprite = sprite
+        this.hitbox = hitbox; // circle by default
     }
 
+    getHitboxCoorX() {
+        return  this.x + 20;
+    }
+    getHitboxCoorY() {
+        return this.y + 10;
+    }
     draw(){
-        ctx.drawImage(this.sprite, this.x, this.y)
+        ctx.drawImage(this.sprite, this.x, this.y);
+        ctx.beginPath();
+        ctx.fillStyle = 'red';
+        ctx.arc(this.getHitboxCoorX(),this.getHitboxCoorY(), this.hitbox, 0, Math.PI * 2, true);
+        ctx.fill();
     }
 
 }
 
 class Player extends Entity {
-    constructor(x, y, sprite, hp, side, velocity) {
+    constructor(x, y, sprite, hp, side, velocity, hitbox) {
         super(x, y, sprite);
         this.hp = hp;
         this.side = side;
         this.velocity = velocity;
+        this.hitbox = hitbox;
     }
 
     update() {
         this.draw();
         this.x = this.x + this.velocity.x;
         this.y = this.y + this.velocity.y;
+        
     }
 }
 
 class Projectile extends Entity {
-    constructor(x, y, sprite, velocity) {
+    constructor(x, y, sprite, velocity, hitbox) {
         super(x, y, sprite);
         this.velocity = velocity;
+        this.hitbox = hitbox;
     }
 
     update() {
         this.draw();
         this.x = this.x + this.velocity.x;
         this.y = this.y + this.velocity.y;
+        //ctx.beginPath();
+        //ctx.arc(this.getHitboxCoorX(),this.getHitboxCoorY(), this.hitbox, 0, Math.PI * 2, true);
+        //  ctx.fill();
     }
 } 
- 
-var player = new Entity(100, 100, bird)
+
+class Blood extends Projectile {
+    constructor(x, y, velocity, hitbox) {
+        super(x, y, null, velocity, hitbox);
+        this.alpha = 2;
+    }
+
+    update() {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        this.alpha -= 0.01
+    }
+
+    draw() {
+        ctx.save();
+        ctx.globalAlpha = this.alpha;
+        ctx.beginPath();
+        ctx.fillStyle = 'red';
+        ctx.arc(this.getHitboxCoorX(),this.getHitboxCoorY(), this.hitbox, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.restore();
+    }
+}
+
+var player = new Player(cvs.width / 2, cvs.height / 2, bird,
+    228, 1, null, 15);
 // some variables
 
 var gap = 85;
@@ -76,28 +119,31 @@ var scor = new Audio();
 fly.src = "sounds/fly.mp3";
 scor.src = "sounds/score.mp3";
 
-const proj1 = new Projectile(cvs.width / 2,
-    cvs.height / 2,
-    bird,
-    {
-        x : 1,
-        y : 1
-    }
-    );
-// on key down
 
-document.addEventListener("keydown",moveUp);
+// on key down
+function keyDownHandler(e) {
+    console.log("Im here");
+    if (e.code == 'ArrowRight') {
+        player.x += 20;
+    }
+    else if (e.code == 'ArrowLeft'){
+        player.x -= 20;
+    }
+    
+}
+document.addEventListener("keydown", keyDownHandler, false );
 document.addEventListener('click', (event) => {
     let angle = Math.atan2(
-        event.clientY - cvs.height / 2,
-        event.clientX - cvs.width / 2
+        event.clientY - player.y,
+        event.clientX - player.x
     );
-    Projectiles.push(new Projectile(cvs.width / 2,
-        cvs.height / 2, bird, 
+    
+    Projectiles.push(new Projectile(player.x,
+        player.y, bird, 
         {
             x : Math.cos(angle),
             y : Math.sin(angle)
-        }));
+        }, 15 )); 
 });
 
 function moveUp(){
@@ -110,6 +156,7 @@ function moveUp(){
 var pipe = [];
 
 var Projectiles = [];
+var Bloods = [];
 var enemies = [];
 pipe[0] = {
     x : cvs.width,
@@ -128,47 +175,90 @@ function spawnEntities() {
             y = Math.random() < 0.5 ? 0 - 20 : cvs.height + 30;
         }
 
-        const angle = Math.atan2(cvs.height / 2 - y, 
-            cvs.width / 2 - x);
+        const angle = Math.atan2(player.y - y, 
+            player.x - x);
 
         const velocity = {
             x : Math.cos(angle),
             y : Math.sin(angle)
         }
         
-        enemies.push( new Player (x, y, bird, 228, 1, velocity));
+        enemies.push( new Player (x, y, bird, 228, 1, velocity, 15));
     }, 1000);
 }
 
+let animationID;
+
 function draw(){
+    animationID = requestAnimationFrame(draw);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
     
-    ctx.clearRect(0, 0, cvs.width, cvs.height);
-    //ctx.drawImage(bg,0,0);
-    
-    
+    Bloods.forEach((blood, idx) => {
+        if (blood.alpha <= 0) {
+            Bloods.splice(idx, 1);
+        } else {
+            blood.update();
+        }
+    });
+
     player.draw();
-    
-        
-        
-    
-
-    ctx.drawImage(fg,0,cvs.height - fg.height);
-    
-    ctx.drawImage(bird,bX,bY);
-    
-    Projectiles.forEach((proj) => {
+    console.log(player.x, player.y);
+    //console.log(enemies);
+    Projectiles.forEach((proj, idx) => {
         proj.update();
+        if (proj.x - proj.hitbox < 0 || proj.getHitboxCoorX() > cvs.width
+            || proj.getHitboxCoorY() < 0 || proj.getHitboxCoorY() > cvs.height) {
+            setTimeout( ()=> {
+                Projectiles.splice(idx, 1);
+            }, 0)
+        }
     });
+    //console.log(Projectiles);
+    enemies.forEach((en, enemies_idx) => {
+        if (en.x > 2500 || en.x < -300 || en.y > 2500 || en.y < -300)
+            enemies.splice(enemies_idx, 1);
+        
+        en.update(1, 1);
+        const dist = Math.hypot(player.getHitboxCoorX() - en.getHitboxCoorX(),
+        player.getHitboxCoorY() - en.getHitboxCoorY());
+        console.log(dist);
+        //handle end game
+        if (dist < 30) {
+            cancelAnimationFrame(animationID);
+        }
+        Projectiles.forEach((projectile, projectl_idx) => {
+            //distance between
+            const dist = Math.hypot(projectile.getHitboxCoorX() - en.getHitboxCoorX(),
+                projectile.getHitboxCoorY() - en.getHitboxCoorY());
+            
+            // handle touch
+            if (dist - en.hitbox - projectile.hitbox < 2) {
+                for (let i = 0; i < 5; i++) {
+                     Bloods.push (
+                         new Blood(projectile.x, projectile. y,
+                            {
+                                x: Math.random() - 0.5,
+                                y: Math.random() - 0.5
+                            }, 3)
+                     );        
+                }
 
-    enemies.forEach((en) => {
-        en.update(1, 1) ;
+                //no flash effect
+                console.log("touch happen");
+                setTimeout(() => {
+                    enemies.splice(enemies_idx, 1);
+                    Projectiles.splice(projectl_idx, 1);
+                }, 0)
+            }
+        })
     });
     
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = "steelblue";
     ctx.font = "20px Verdana";
-    ctx.fillText("Score : "+score,10,cvs.height-20);
+    ctx.fillText("Score : "+ score, 10, 30);
     
-    requestAnimationFrame(draw);
+    
     
 }
 
@@ -176,27 +266,4 @@ function draw(){
 
 draw();
 spawnEntities();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
